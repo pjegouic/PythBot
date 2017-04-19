3da3bc6c478f211bdc69b0fb99c36fa7cedf7b95
Paul JEGOUIC <p.jegouic@lescomptoirs.com>
2017-04-19 15:14:15 +0200

    ezaeza

--- bot.py
+++ bot.py
@@ -1,30 +1,32 @@
 from wit import Wit
-from flask import Flask
+from flask import Flask,request, make_response
 import logging
 import requests
 import json
+import sys
 
 
 # Definition des constantes
 app = Flask(__name__)
 WIT_ACCESS_TOKEN = '45O73DNGA4W6YBPS3GXJXAPTZK725XXE'
 
-@app.route ('/webhook/', method=['GET'])
+@app.route ('/webhook/', methods=['GET'])
 def webhook_get():
+    sys.stderr.write('Verify Token ' + request.args.get('hub.verify_token'))
     if request.args.get('hub.verify_token') == 'TOTO':
         return request.args.get('hub.challenge')
     else:
         return request.args.get('Error, Wrong Token')
 
 
-@app.route('/webhook/', method=['POST'])
+@app.route('/webhook/', methods=['POST'])
 def webhook_post():
     print(request.data[0].messaging)
     messaging_events = request.data[0].messaging
     for i in messaging_events :
         event = request.data[0].messaging[i]
         sender = event.sender.id
-        if event.message and event.message.text
+        if event.message and event.message.text:
             text = event.message.text
             if text == 'Generic' :
                 logging.info('Welcome to PythBot')
@@ -40,7 +42,7 @@ def webhook_post():
 
 #FACEBOOK PART
 
-FB_TOKEN = 'TOTOTO'
+FB_TOKEN = 'EAAD7Jwopoh4BAO6j1yJmn7ZAS3ikq6qQCgd5QqygxLlkO0LSEYPX9V3jSZADYIpJJPdNM6Ketmmgpg9U9MmVXbzWNk9sSf1W5ywKzSFVVplf1xouk9014HU7iDYEbK21mYmAa2pZC7kMxQzKu7ZBXxk73G2wIeTpamk4B5UPZBQZDZD'
 fb_url = 'https://graph.facebook.com/v2.6/me/messages'
 
 
