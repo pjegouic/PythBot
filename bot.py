da4dc6764b2417d0fcb0b2b1e5fcff8cbd39d391
Paul JEGOUIC <p.jegouic@lescomptoirs.com>
2017-04-19 17:29:07 +0200

    z

--- bot.py
+++ bot.py
@@ -22,7 +22,8 @@ def webhook_get():
 def webhook_post():
     sys.stderr.write(request.data)
     req = json.loads(request.data)
-    sys.stdout.write(req)
+    sys.stdout.write(type(req))
+    sys.stdout.write(json.dumps(req))
     messaging_events = req.entry[0].messaging
     for i in messaging_events :
         event = req.entry[0].messaging[i]
