0b7598d3b459acfbdabcd668ab43f3ab00a02813
Paul JEGOUIC <p.jegouic@lescomptoirs.com>
2017-04-19 18:10:38 +0200

    z

--- bot.py
+++ bot.py
@@ -32,7 +32,7 @@ def webhook_post():
                 sys.stderr.write('Welcome to PythBot')
                 continue
             sendTextMessage(sender, "Text received, echo : " + text)
-        if event['postback'] :
+        if event['postback'] is not None :
             text = json.loads(event.postback)
             sendTextMessage(sender, "Text received, echo : " + text, FB_TOKEN)
             continue
