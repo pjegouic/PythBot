40b4a330482dcce7e6a8c3edca6f5695f4b8af06
Paul JEGOUIC <p.jegouic@lescomptoirs.com>
2017-04-20 17:44:50 +0200

    added conversation

--- bot.py
+++ bot.py
@@ -8,10 +8,10 @@ LANGUAGE = 'fr'
 def analyse_text(sender,payload):
     request = recastai.Request(token=RECAST_TOKEN)
     response = request.analyse_text(payload)
-    return "TEXT INPUT SOURCE : " + str(response.source) + "\n" + "RESULTAT ANALYSE RECAST" +str(response.intents)
+    return response.raw
 
 def converse(sender,payload):
     request = recastai.Request(token=RECAST_TOKEN)
     response = request.converse_text(payload)
-    return jsonify(response)
+    return response.raw
 
